19c9d6b32935a934f4f3a596f4cd1afdc481b1e4
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-11-18 10:44:05 +0530

    Write Function for DB connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,34 @@
+// In NextJS, the server is not running every time it runs on demand. So we have to take care of the things like if we are already connected to the database or not.
+
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+// Connect to MongoDB
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to DB");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+
+    console.log("DB", db);
+    console.log("DB.connections", db.connections);
+    
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Database connection error", error);
+
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
