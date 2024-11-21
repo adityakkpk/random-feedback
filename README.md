fef2ce664a6dcda51545d4cc2b5a86a46890175b
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-11-21 19:59:07 +0530

    Crash course on Next Auth or Authjs

--- README.md
+++ README.md
@@ -10,11 +10,12 @@ Using ZOD for validation.
 
 In NextJS, the server is not running every time it runs on demand. So we have to take care of the things like if we are already connected to the database or not.
 
-### Register and Sign up Algorithm  
+### Register and Sign up Algorithm
 
 Code should effectively handle both scenarios of registering a new user and updating an existing but unverified user account with a new password and verification code.
 
-* *Algorithm*
+- _Algorithm_
+
 ```
 IF existingUserByEmail EXISTS THEN
     IF existingUserByEmail.isVerified THEN
@@ -28,5 +29,52 @@ ELSE
 END IF
 ```
 
-* Using `resend email` for email verification
-* Using `react email` for email templates
+- Using `resend email` for email verification
+- Using `react email` for email templates
+
+- We are using `next-auth` for signup.
+- We are simply using credentials to sign up the user. For that we are using `credentials` feature from Next-auth.
+
+```javascript
+providers: [
+    // Here we are creating a component for sign up. By using this below information, next-auth will create a component for signup which will render on the screen
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "Enter password",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter password",
+        },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+            const user = await UserModel.findOne({
+                $or: [
+                    {email: credentials.identifier},
+                    {username: credentials.identifier}
+                ]
+            })
+
+            if (!user) {
+              throw new Error("No user found")
+            }
+
+            if(!user.isVerified) {
+              throw new Error("Please verify your account before login.")
+            }
+
+        } catch (error: any) {
+            throw new Error(error);
+        }
+      },
+    }),
+  ],
+```
---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,85 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "Enter password",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter password",
+        },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("No user found.");
+          }
+
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before login.");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isPasswordCorrect) {
+            throw new Error("Password is incorrect.");
+          } else {
+            return user;
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.username = token.username;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessage = token.isAcceptingMessage;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.username = user.username;
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessage = user.isAcceptingMessage;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
